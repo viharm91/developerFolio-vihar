51218e655f8d04166d57a71ff562a33fad9f16d5
viharm91 <47459985+viharm91@users.noreply.github.com>
2024-10-13 14:41:00 -0400

    Update Greeting.js vihar profile update

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -57,7 +57,7 @@ export default function Greeting() {
             ) : (
               <img
                 alt="man sitting on table"
-                src={require("../../assets/images/manOnTable.svg")}
+                src={require("../../assets/images/Vihar.jpg")}
               ></img>
             )}
           </div>
